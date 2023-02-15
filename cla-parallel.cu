44bd00303ad8e1b3f0ef2509f5ce277c49548ab9
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-02-14 22:53:44 -0500

    Update cla-parallel.cu

--- cla-parallel.cu
+++ cla-parallel.cu
@@ -440,8 +440,8 @@ void check_cla_rca()
 	{
 	  printf("Check: Found sumrca[%d] = %d, not equal to sumi[%d] = %d - stopping check here!\n",
 		 i, sumrca[i], i, sumi[i]);
-	  printf("bin1[%d] = %d, bin2[%d]=%d, gi[%d]=%d, pi[%d]=%d, ci[%d]=%d, ci[%d]=%d\n",
-		 i, bin1[i], i, bin2[i] i, gi[i], i, pi[i], i, ci[i], i-1, ci[i-1]);
+     printf("bin1[%d] = %d, bin2[%d]=%d, gi[%d]=%d, pi[%d]=%d, ci[%d]=%d, ci[%d]=%d\n",
+		 i, bin1[i], i, bin2[i], i, gi[i], i, pi[i], i, ci[i], i-1, ci[i-1]);
 	  return;
 	}
     }
